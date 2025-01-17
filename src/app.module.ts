cfc80b563d6265af4aedf24f18f8aab20779d301
JaeYoung <kimjaeyoung650@gmail.com>
2025-01-17 15:57:05 +0900

    Feat: NestJS와 Mongoose 연동을 위한 패키지 수정 및 기본 환경설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PrismaModule } from 'prisma/prisma.module';
 import { CrawlingModule } from './crawling/crawling.module';
-
 
 @Module({
   imports: [
     PrismaModule,
     CrawlingModule,
+    MongooseModule.forRoot(process.env.DATABASE_URL),
   ],
 })
 
--- src/crawling/crawling.module.ts
+++ src/crawling/crawling.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PrismaModule } from 'prisma/prisma.module';
 import { CrawlerService } from './crawler.service';
 import { CrawlingService } from './crawling.service';
 import { CrawlingController } from './crawling.controller';
 import { CrawlingRepository } from './crawling.repository';
-import { PrismaModule } from 'prisma/prisma.module';
+import { CrawledNews, CrawledNewsSchema } from './schemas/crawling.schema';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    MongooseModule.forFeature([{ name: CrawledNews.name, schema: CrawledNewsSchema }]),
+  ],
   providers: [
     CrawlerService,
     CrawlingService,
