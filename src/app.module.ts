9b6ab20e884929f4bc22af0cae4ec6edfb6bf3cd
JaeYoung <kimjaeyoung650@gmail.com>
2025-01-20 23:51:37 +0900

    Feat: 스케줄러 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PrismaModule } from 'prisma/prisma.module';
 import { CrawlingModule } from './crawling/crawling.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
   imports: [
     PrismaModule,
     CrawlingModule,
-    ScheduleModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    ScheduleModule.forRoot(),
+    SchedulerModule,
   ],
 })
 
