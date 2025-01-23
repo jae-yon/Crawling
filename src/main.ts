753fcc883d5dbf8bc7a509c8c911232265f16661
JaeYoung <kimjaeyoung650@gmail.com>
2025-01-23 16:27:37 +0900

    Feat: CORS 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: configService.get<string>('FRONT_URL').split(', '),
+    methods: configService.get<string>('HTTP_METHODS').split(', '),
+    allowedHeaders: configService.get<string>('ALLOWED_HEADERS').split(', '),
+    credentials: true,
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
